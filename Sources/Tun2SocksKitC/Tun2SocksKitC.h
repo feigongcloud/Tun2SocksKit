216eec9f9a3c2dc377e203e8bdb4ba4ec4e5b356
土豆喵 <feigong@malegobi.com>
2025-03-01 22:46:49 +0800

    Update Tun2SocksKitC.h

--- Sources/Tun2SocksKitC/Tun2SocksKitC.h
+++ Sources/Tun2SocksKitC/Tun2SocksKitC.h
@@ -14,7 +14,7 @@ struct ctl_info {
 struct sockaddr_ctl {
     u_char      sc_len;       // 使用 u_char 与系统一致
     u_char      sc_family;
-    uint16_t    ss_sysaddr;
+    u_int16_t    ss_sysaddr;
     u_int32_t   sc_id;        // 同样使用 u_int32_t
     u_int32_t   sc_unit;
     u_int32_t   sc_reserved[5];

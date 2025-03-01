dc42219fdf1f218c6f63561c7ed4e643c652ce26
土豆喵 <feigong@malegobi.com>
2025-03-01 22:45:15 +0800

    Update Tun2SocksKitC.h

--- Sources/Tun2SocksKitC/Tun2SocksKitC.h
+++ Sources/Tun2SocksKitC/Tun2SocksKitC.h
@@ -1,21 +1,23 @@
 #ifndef Tun2SocksKitC_H
 #define Tun2SocksKitC_H
 
-#include <stdint.h>
+
+#include <sys/types.h>
 
 #define CTLIOCGINFO 0xc0644e03UL
 
 struct ctl_info {
-    uint32_t   ctl_id;
+    u_int32_t   ctl_id;       // 使用 u_int32_t 而不是 uint32_t
     char        ctl_name[96];
 };
+
 struct sockaddr_ctl {
-    unsigned char      sc_len;
-    unsigned char      sc_family;
-    uint16_t   ss_sysaddr;
-    uint32_t   sc_id;
-    uint32_t   sc_unit;
-    uint32_t   sc_reserved[5];
+    u_char      sc_len;       // 使用 u_char 与系统一致
+    u_char      sc_family;
+    uint16_t    ss_sysaddr;
+    u_int32_t   sc_id;        // 同样使用 u_int32_t
+    u_int32_t   sc_unit;
+    u_int32_t   sc_reserved[5];
 };
 
 #endif /* Tun2SocksKitC_H */

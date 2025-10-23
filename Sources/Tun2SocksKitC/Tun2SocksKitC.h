8f5ad9215931d839a5658c93fd9cf7aa8d78d4d3
土豆喵 <feigong@malegobi.com>
2025-10-23 15:39:38 +0800

    Add SOCKS5 tunnel functions and connection callback
    Added function declarations for SOCKS5 tunnel management and connection callbacks.

--- Sources/Tun2SocksKitC/Tun2SocksKitC.h
+++ Sources/Tun2SocksKitC/Tun2SocksKitC.h
@@ -19,3 +19,23 @@ struct sockaddr_ctl {
 };
 
 #endif /* Tun2SocksKitC_H */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void (*OnNewConnectionCallback)(const char *ip, int port);
+
+/* Swift 调用此函数注册回调 */
+void Tun2SocksKitC_SetOnNewConnection(OnNewConnectionCallback cb);
+
+/* 转发到底层 HevSocks5Tunnel */
+int32_t hev_socks5_tunnel_main(const char *config_path, int32_t tun_fd);
+int32_t hev_socks5_tunnel_main_from_str(const char *config_str, uint32_t len, int32_t tun_fd);
+void hev_socks5_tunnel_stats(int *tx_packets, int *tx_bytes, int *rx_packets, int *rx_bytes);
+void hev_socks5_tunnel_quit(void);
+void hev_socks5_tunnel_set_on_new_connection(OnNewConnectionCallback cb);
+
+#ifdef __cplusplus
+}
+#endif

d4f284180caa67bb3ff04d5cb940e135193b1349
土豆喵 <feigong@malegobi.com>
2025-10-23 15:40:17 +0800

    Add function to set new connection callback

--- Sources/Tun2SocksKitC/Tun2SocksKitC.c
+++ Sources/Tun2SocksKitC/Tun2SocksKitC.c
@@ -1,1 +1,7 @@
 // empty
+#include "Tun2SocksKitC.h"
+#include <HevSocks5Tunnel/hev-socks5-tunnel.h>
+
+void Tun2SocksKitC_SetOnNewConnection(OnNewConnectionCallback cb) {
+    hev_socks5_tunnel_set_on_new_connection(cb);
+}
